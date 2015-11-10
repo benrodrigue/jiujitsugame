f44a7393893c2f6163c0e21570a39f043282f1ed
benrodrigue <ben.rodrigue@gmail.com>
2015-11-10 19:33:31 +0000

    Hourly Commit

--- bjj.py
+++ bjj.py
@@ -1,7 +1,21 @@
-
-
+#! /usr/bin/env python3
 #Jiu Jitsu Game
 
+import random
 
-closed guard, I sit up on my opponent and now my options are
-1) Hip-bump sweep 2) Kimura or 3) Guillotine
+offense = {
+    "takedowns": ["single leg", "double leg", "judo throw"],
+    "inguard": ["
+}
+
+defense = {
+    "takedowns": ["sprawl"], 
+    
+}
+
+
+# closed guard, I sit up on my opponent and now my options are
+# 1) Hip-bump sweep 2) Kimura or 3) Guillotine
+# difficulty of move may impact success - weighted against experience level
+action = input("What do you do?")
+
