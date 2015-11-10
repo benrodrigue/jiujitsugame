8d52569a5a8d6da8bc4dabe4a6cbd221ebef7b62
benrodrigue <ben.rodrigue@gmail.com>
2015-11-10 22:48:35 +0000

    tuesday push

--- bjj.py
+++ bjj.py
@@ -2,19 +2,23 @@
 #Jiu Jitsu Game
 
 import random
+import position_lists
 
-current_state = ()
+position = ()
 
 offense = {
-    "takedowns": ["single leg", "double leg", "judo throw"],
+    "takedowns": ["single leg", "double leg", "judo throw", "pull guard"],
     "closedguardbottom": ["hip-bump sweep", "kimura", "guillotine"],
     "closedguardtop": ["arm lock", "key lock", "cross choke"],
     "backmount": ["rear naked choke"]
 }
 
 defense = {
-    "takedowns": ["sprawl"], 
-    "closedguard": [ ]
+    "takedowns": ["sprawl", "pull guard"], 
+    "closedguardbottom": ["shrimp", "cross arms by neck"],
+    "closedguardtop": ["posture up and defend","stand up guard pass"],
+    "backmount": ["rear naked choke defense", "turtle"],
+    "tap": ["tap out"]
     
 }
 
@@ -27,14 +31,24 @@ decide to do. As you advance, you will be more successful.
 
 if input("Shall we begin?").lower() in ["y", "yes"]:
     print("Good! You bow as you enter the mat and you bow and shake your training partners hand.")
+    print(position_lists.play_pos_top["a"] + " and " + position_lists.opp_pos_bot["a"])
     print("You both begin circling...")
+    print
 else:
     print("Oh, keep watching youtube and come back when you are ready.")
     exit
     
+for description, move in offense.items():
+    print(range("takedowns"))
+    action = input("What do you do?") 
+    
+
+position = position_lists.play_pos_bot["a"] + " and " + position_lists.opp_pos_top["a"]
+print(position)
+
+
 
 # closed guard, I sit up on my opponent and now my options are
 # 1) Hip-bump sweep 2) Kimura or 3) Guillotine
 # difficulty of move may impact success - weighted against experience level
-action = input("What do you do?")
 
---
+++ position_lists.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+#Dictionary of positions used in bjj.py 
+
+play_pos_top = {
+    "a": "You are standing up in fight stance",
+    "b": "You have top mount", 
+    "c": "You have your opponent in side control",
+    "d": "You are on top and have your opponent in half guard.",
+    "e": "You have taken your opponents back.",
+    
+}
+play_pos_bot = {
+    "a": "You are on bottom on your back",
+    "b": "You are on bottom on your hands and knees", 
+}
+
+opp_pos_top = {
+    "a": "your opponent is in top mount", 
+    "b": "your opponent has you in side control",
+    "c": "your opponent is on top and has you in half guard.",
+    "d": "your opponent has taken your back.",
+}
+opp_pos_bot = {
+    "a": "your opponent is in fight stance.",
+    "b": "your opponent is on his back.",
+    "c": "your opponent is on his hands and knees.",
+}
+    
+
